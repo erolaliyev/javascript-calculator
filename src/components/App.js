15113db1f4f0c117dfc1762d1e80b05a3659e3de
Erol Aliyev <aliyeverol98@gmail.com>
2019-07-09 00:47:03 +0200

    Done :tada:

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,6 @@
 import React, { useState } from 'react';
 import NumberPad from './NumberPad';
 
-// const buttons = [
-//   `1`,
-//   `2`,
-//   `3`,
-//   `4`,
-//   `5`,
-//   `6`,
-//   `7`,
-//   `8`,
-//   `9`,
-//   `0`,
-//   `.`,
-//   `+`,
-//   `-`,
-//   `x`,
-//   `/`,
-//   `AC`,
-//   `=`
-// ];
-
 const buttons = [
   {
     value: `1`,
@@ -75,9 +55,17 @@ const isOperationSign = el => {
   if (el === `+` || el === `-` || el === `x` || el === `/` || el === `*`)
     return true;
 };
+
 const App = () => {
   const [display, setDisplay] = useState(`0`);
   const [isDisplayFloat, changeDisplayToFloat] = useState(false);
+  const digitLimitMet = () => {
+    setDisplay(`Digit Limit Met`);
+    setTimeout(function() {
+      setDisplay(`0`);
+      changeDisplayToFloat(false);
+    }, 500);
+  };
   const handleClick = e => {
     const clickedButton = e.target.innerHTML;
     const lastCharacterOfDisplay = display.toString().slice(-1);
@@ -116,55 +104,79 @@ const App = () => {
           let result = 0;
           if (isOperationSign(lastCharacterOfDisplay)) {
             result = eval(display.substring(0, display.length - 1));
-            setDisplay(result.toString());
+            if (result.toString().length > 20) {
+              digitLimitMet();
+            } else {
+              setDisplay(result.toString());
+              changeDisplayToFloat(result % 1);
+            }
           } else {
             result = eval(display);
-            setDisplay(result.toString());
+            if (result.toString().length > 20) {
+              digitLimitMet();
+            } else {
+              setDisplay(result.toString());
+              changeDisplayToFloat(result % 1);
+            }
           }
-          changeDisplayToFloat(result % 1);
         } else if (isOperationSign(clickedButton)) {
-          if (isOperationSign(lastCharacterOfDisplay)) {
-            if (clickedButton === `x`) {
-              setDisplay(display.substring(0, display.length - 1) + `*`);
-            } else {
-              setDisplay(
-                display.substring(0, display.length - 1) + clickedButton
-              );
-            }
+          if (display.length > 19) {
+            digitLimitMet();
           } else {
-            if (clickedButton === `x`) {
-              setDisplay(display + `*`);
+            if (isOperationSign(lastCharacterOfDisplay)) {
+              if (clickedButton === `x`) {
+                setDisplay(display.substring(0, display.length - 1) + `*`);
+              } else {
+                setDisplay(
+                  display.substring(0, display.length - 1) + clickedButton
+                );
+              }
             } else {
-              setDisplay(display + clickedButton);
+              if (clickedButton === `x`) {
+                setDisplay(display + `*`);
+              } else {
+                setDisplay(display + clickedButton);
+              }
             }
+            changeDisplayToFloat(false);
           }
-          changeDisplayToFloat(false);
         } else if (clickedButton === `.`) {
-          if (isOperationSign(lastCharacterOfDisplay)) {
-            setDisplay(display + `0` + clickedButton);
-            changeDisplayToFloat(true);
+          if (display.length > 19) {
+            digitLimitMet();
           } else {
-            if (!isDisplayFloat) {
-              setDisplay(display + clickedButton);
+            if (isOperationSign(lastCharacterOfDisplay)) {
+              setDisplay(display + `0` + clickedButton);
               changeDisplayToFloat(true);
             } else {
-              setDisplay(display);
+              if (!isDisplayFloat) {
+                setDisplay(display + clickedButton);
+                changeDisplayToFloat(true);
+              } else {
+                setDisplay(display);
+              }
             }
           }
         } else {
           if (display === `0`) {
             setDisplay(clickedButton);
           } else {
-            setDisplay(display + clickedButton);
+            if ((display + clickedButton).length > 20) {
+              digitLimitMet();
+            } else {
+              setDisplay(display + clickedButton);
+            }
           }
         }
       }
     } else {
       setDisplay(`0`);
+      if (isDisplayFloat) {
+        changeDisplayToFloat(false);
+      }
     }
   };
   return (
-    <div>
+    <div id='container'>
       <p id='display'>{display}</p>
       {buttons.map(e => (
         <NumberPad key={e.id} value={e.value} id={e.id} onClick={handleClick} />
--- src/components/NumberPad.js
+++ src/components/NumberPad.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const NumberPad = props => {
-  return (
-    <div>
-      <button onClick={props.onClick} id={props.id}>
-        {props.value}
-      </button>
-    </div>
-  );
+const NumberPad = props => (
+  <button onClick={props.onClick} id={props.id}>
+    {props.value}
+  </button>
+);
+
+NumberPad.propTypes = {
+  onClick: PropTypes.func,
+  id: PropTypes.string,
+  value: PropTypes.string
 };
 
 export default NumberPad;
